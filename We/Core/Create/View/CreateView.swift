1aa145e333194a02077c7f402debe1f417ec4374
Om Preetham <ompreethambandi@gmail.com>
2024-08-10 22:16:21 -0500

    updated create view design

--- We/Core/Create/View/CreateView.swift
+++ We/Core/Create/View/CreateView.swift
@@ -11,35 +11,50 @@ struct CreateView: View {
     @Environment(\.dismiss) var dismiss
     
     @State private var caption = ""
+    @State private var selectedCommunity: Community = Community.mockCommunities[0]
     
-    var community: Community? = .mockCommunities[0]
+    var communities: [Community]? = Community.mockCommunities
+    var user: User? = User.mockUsers[0]
     
     var body: some View {
         NavigationStack {
             VStack {
-                HStack(alignment: .top) {
-                    CircularProfileImageView(community: .mockCommunities[0], size: .small)
+                HStack(alignment: .top, spacing: 0) {
+                    CircularProfileImageView(community: selectedCommunity, size: .medium)
+                        .offset(y: 8)
                     
                     VStack(alignment: .leading, spacing: 4) {
-                        Text(community?.name ?? "Community")
-                            .fontWeight(.semibold)
+                        HStack(spacing: 2) {
+                            Picker("Select Community", selection: $selectedCommunity) {
+                                ForEach(communities ?? [], id: \.id) { community in
+                                    Text(community.name.capitalized)
+                                        .tag(community)
+                                }
+                            }
+                            .pickerStyle(.menu)
+                            
+                            Text(user?.username ?? "UserName")
+                                .font(.footnote)
+                                .foregroundStyle(.secondary)
+                            
+                            Spacer()
+                            
+                            if !caption .isEmpty {
+                                Button {
+                                    caption = ""
+                                } label: {
+                                    Image(systemName: "xmark")
+                                        .resizable()
+                                        .tint(.secondary)
+                                        .frame(width: 12, height: 12)
+                                }
+                            }
+                        }
                         
                         TextField("Start writing", text: $caption, axis: .vertical)
+                            .padding(.leading, 12)
                     }
                     .font(.subheadline)
-                    
-                    Spacer()
-                    
-                    if !caption .isEmpty {
-                        Button {
-                            caption = ""
-                        } label: {
-                            Image(systemName: "xmark")
-                                .resizable()
-                                .tint(.secondary)
-                                .frame(width: 12, height: 12)
-                        }
-                    }
                 }
                 
                 Spacer()
@@ -47,6 +62,7 @@ struct CreateView: View {
             .padding()
             .navigationTitle("New Post")
             .navigationBarTitleDisplayMode(.inline)
+            .interactiveDismissDisabled()
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
                     Button("Cancel") {
