97aafbcee74940b558ff6731f3e321bbedfb7646
Om Preetham Bandi <ompreethambandi@gmail.com>
2024-10-03 21:42:19 -0500

    Initial Commit

---
+++ We/WeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeApp.swift
+//  We
+//
+//  Created by Om Preetham Bandi on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
